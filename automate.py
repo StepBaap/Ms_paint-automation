adf8617e07685cc242c516b24710ee2792215c15
StepBaap <87863471+StepBaap@users.noreply.github.com>
2021-08-28 11:50:35 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -3,15 +3,15 @@ import numpy as np
 import time
 import cv2
 
-def auto_canny(image, sigma=0.33):
-	# compute the median of the single channel pixel intensities
-	v = np.median(image)
+def auto_thresh_canny(image, sigma=0.33):
+	# compute the median of image pixels 
+	med = np.median(image)
 	# apply automatic Canny edge detection using the computed median
-	lower = int(max(0, (1.0 - sigma) * v))
-	upper = int(min(255, (1.0 + sigma) * v))
-	edged = cv2.Canny(image, lower, upper)
+	low = int(max(0, (1.0 - sigma) * med))
+	high = int(min(255, (1.0 + sigma) * med))
+	edge = cv2.Canny(image, low, high)
 	# return the edged image
-	return edged
+	return edge
 
 def Resize(image,w):
 	
@@ -29,7 +29,7 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection
 img_blur = cv2.GaussianBlur(img_gray, (3,3), 0) 
 # Apply Canny Edge Algo using optimal threshholds
-edge = auto_canny(img_blur)
+edge = auto__thresh_canny(img_blur)
 # Resize the edged image
 w = 800
 edge,h = Resize(edge,w)
@@ -45,13 +45,13 @@ time.sleep(2)
 
 gui.click(808,87)    		 # coordinates of fill icon
 gui.click(268,70)    		 # coordinates of background colour location
-gui.click(237,221) 	 		 # coordinates on a random location on the canvas for filling	
-gui.click(242,104)	 		 # coordinates for selecting the eraser tool
-gui.click(638,94)	 		 # coordinates for size dropdown tool
+gui.click(237,221) 	         # coordinates on a random location on the canvas for filling	
+gui.click(242,104)	   	 # coordinates for selecting the eraser tool
+gui.click(638,94)	 	 # coordinates for size dropdown tool
 gui.moveTo(639,125)  		 # coordinates of eraser size
 gui.click(button='right')
-gui.click(628,128,clicks=2)  # coordinates of lowest eraser size 
-gui.move(8,164)# random coordinates on the canvas
+gui.click(628,128,clicks=2)  	 # coordinates of lowest eraser size 
+gui.move(8,164)			 # random coordinates on the canvas
          
 # relative coordinates for the image to be drawn in the canvas         
 x0,y0 = 237,221          
